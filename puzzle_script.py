caec12ee4058a6223f9b92225a28702cb862b270
Patrick Henson <patrick.henson@outlook.com>
2021-09-06 02:55:02 -0400

    read input from file, apply cipher, output with image and custom font

--- puzzle_script.py
+++ puzzle_script.py
@@ -1,52 +1,132 @@
 import argparse
+import os
 import random
 import string
+import sys
+from PIL import Image, ImageDraw, ImageFont
 
-cipher_key = {}
-
-def generate_key(seed, key):
-    # Create dictionary with each letter of the alphabet to track which letters have been shuffled.
-    # alphabet = {}
-    # for letter in list(string.ascii_lowercase):
-    #     alphabet[letter] = False
-
-    # Track which letters have been used in the generated key
+# Generate the cipher key using a set seed value.
+# The generated key is stored as a dictionary with [key,value] pairs as [letter,substitution]
+def generate_key(seed):
+    # Track which letters have been used in the generated key.
     alphabet = string.ascii_lowercase
     alphabet_length = len(alphabet)
     letter_is_used = [False] * len(alphabet)
 
-    # # Seeding the random algorithm results allows the same key to be used for multiple messages, 
-    # # as long as they use the same seed input.
-    # random.seed(seed)
-    # for letter in alphabet.keys: 
-    #     shuffled = False
-    #     while not shuffled:
-    #         new_position = random.randint(0, 26)
-    #         if not alphabet[new_position]:
-    #             key[letter]
-
-    print("generate key")
+    # Seeding the random number generator allows the same key to be used for multiple messages.
     random.seed(seed)
+
+    # Assign a unique replacement to each letter of the alphabet.
+    cipher_key = {}
     for i in range(0, alphabet_length):
-        shuffled = False
-        while not shuffled:
+        substituted = False
+        while not substituted:
             new_position = random.randint(0, alphabet_length-1)
             if not letter_is_used[new_position] and i is not new_position:
-                key[alphabet[i]] = alphabet[new_position]
+                cipher_key[alphabet[i]] = alphabet[new_position]
                 letter_is_used[new_position] = True
-                shuffled = True
+                substituted = True
+    return cipher_key
 
-    print("key done")
-    print(key)
+# Generate a new dictionary by reversing the [key,value] pairs of the key.
+# This function is used for proof/testing and is not exposed.
+def reverse_key(cipher_key):
+    reversed_key = {}
+    for k in cipher_key.keys():
+        reversed_key[cipher_key[k]] = k
+    return reversed_key
+
+# Encode the message using the substitution cipher key.
+# The encoded message will only contain lowercase letters.
+def encode_substitution_cipher(message, cipher_key):
+    encoded_message = ""
+    for c in message.lower():
+        if c in cipher_key.keys():
+            encoded_message += cipher_key[c]
+        else:
+            encoded_message += c
+    return encoded_message
+    
+
+# Decode the message using the substitution cipher key.
+# This function is used for proof/testing and is not exposed.
+def decode_substitution_cipher(message, cipher_key):
+    reversed_key = reverse_key(cipher_key)
+    decoded_message = ""
+    for c in message.lower():
+        if c in reversed_key.keys():
+            decoded_message += reversed_key[c]
+        else:
+            decoded_message += c
+    return decoded_message
+
+# Generate image output 
+def generate_image(message):
+    # Create base image.
+    image_size = (600,800)
+    canvas = Image.new('RGB', image_size, 'white')
+    # Load custom font and render message.
+    font_file = os.path.abspath(os.getcwd()) + '/fonts/tuigan_font/Tuigan-KppX.ttf'
+    font = ImageFont.truetype(font_file, size=14, encoding='unic')
+    render = ImageDraw.Draw(canvas)
+    render.text((10,10), message, 'black', font)
+
+    # canvas.save('output_image.jpg')
+    canvas.show()
 
 
+# Processes command line input and execute the program.
 def main(): 
-    print('Use puzzle_script to prepare messages for your RPG.  See --help for more information.')
+    # Program description
+    description_text='''
+    Welcome to Puzzle Script - Substitution Cipher\n
 
-    parser = argparse.ArgumentParser("Use puzzle_script to prepare messages for your RPG.\nThis program allows you to apply a substitution cipher and alternative fonts to your messages to add depth and character to your game.")
-    parser.add_argument('--input', metavar='', type=str, help='TXT input file containing the message to process.  Default is message.txt if empty')
+    This code was developed to easily generate 'secret messages' for use in tabletop role playing games.  The output 
+    can be given to players to decode on their own or the key can be provided to give them a head start.
+    '''
 
-    generate_key(255, cipher_key)
+    # Setup command line input
+    parser = argparse.ArgumentParser(description=description_text)
+    parser.add_argument(
+        '-f', 
+        dest='file_input',
+        type=str,
+        default='message.txt',
+        help='Input file containing the message(s) to process.  Default is "message.txt".')
+    parser.add_argument(
+        '-s',
+        dest='seed',
+        type=int,
+        required=False,
+        help='Set the seed value to gaurantee the same random cipher key is used across multiple runs.'
+    )
+    
+    # Parse command line input.
+    args = parser.parse_args()
+    
+    # Read message from file.
+    text_input = open(args.file_input, 'r')
+    message = text_input.read()
+    text_input.close()
+    
+    # Generate the substitution cipher key.
+    seed = args.seed
+    if seed is None:
+        seed = random.randint(0, sys.maxsize)
+    cipher_key = generate_key(seed)
+
+    # Encode the message
+    encoded_message = encode_substitution_cipher(message, cipher_key)
+
+    # Generate image containing the message text.
+    generate_image(encoded_message)
+
+    # Output a file with process information.
+    log = '## RANDOM SEED ##\n{}\n\n## MESSAGE ##\n{}\n\n## ENCODED MESSAGE ##\n{}\n\n'
+    text_output = open('output.txt', 'w')
+    text_output.write(log.format(seed, message, encoded_message))
+    text_output.close()
+
 
 if __name__ == "__main__":
     main()
--- test/test_puzzle_script.py
+++ test/test_puzzle_script.py
@@ -1,4 +1,4 @@
-from puzzle_script.puzzle_script import generate_key
+from puzzle_script.puzzle_script import decode_substitution_cipher, encode_substitution_cipher, generate_key, reverse_key
 import sys
 sys.path.append("../")
 
@@ -7,9 +7,34 @@ from puzzle_script import *
 
 # Test the generate_key function reliable creates the same key for the seed input.
 def test_generate_key():
-    expected_key = {'a': 'b', 'b': 'k', 'c': 'u', 'd': 'z', 'e': 's', 'f': 'l', 'g': 't', 'h': 'y', 'i': 'e', 'j': 'q', 'k': 'v', 'l': 'r', 'm': 'i', 'n': 'm', 'o': 'j', 'p': 'o', 'q': 'f', 'r': 'n', 's': 'c', 't': 'g', 'u': 'w', 'v': 'h', 'w': 'x', 'x': 'd', 'y': 'p', 'z': 'a'}
-
-    cipher_key = {}
-    generate_key(255, cipher_key)
+    expected_key = {
+        'a': 'b', 'b': 'k', 'c': 'u', 'd': 'z', 'e': 's', 'f': 'l', 'g': 't', 'h': 'y', 'i': 'e', 'j': 'q', 'k': 'v', 
+        'l': 'r', 'm': 'i', 'n': 'm', 'o': 'j', 'p': 'o', 'q': 'f', 'r': 'n', 's': 'c', 't': 'g', 'u': 'w', 'v': 'h', 
+        'w': 'x', 'x': 'd', 'y': 'p', 'z': 'a'
+    }
+    cipher_key = generate_key(255)
     assert cipher_key == expected_key
     
+def test_reverse_key():
+    expected_key = {
+        'b': 'a', 'k': 'b', 'u': 'c', 'z': 'd', 's': 'e', 'l': 'f', 't': 'g', 'y': 'h', 'e': 'i', 'q': 'j', 'v': 'k', 
+        'r': 'l', 'i': 'm', 'm': 'n', 'j': 'o', 'o': 'p', 'f': 'q', 'n': 'r', 'c': 's', 'g': 't', 'w': 'u', 'h': 'v', 
+        'x': 'w', 'd': 'x', 'p': 'y', 'a': 'z'
+    }
+    cipher_key = generate_key(255)
+    reversed_key = reverse_key(cipher_key)
+    assert reversed_key == expected_key
+
+def test_encode_substitution_cipher():
+    input_message = 'Hello World!'
+    expected_message = 'ysrrj xjnrz!'
+    cipher_key = generate_key(255)
+    encoded_message = encode_substitution_cipher(input_message, cipher_key)
+    assert encoded_message == expected_message
+
+def test_decode_substitution_cipher():
+    input_message = 'ysrrj xjnrz!'
+    expected_message = 'hello world!'
+    cipher_key = generate_key(255)
+    decoded_message = decode_substitution_cipher(input_message, cipher_key)
+    assert decoded_message == expected_message
